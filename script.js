10e235e09e905281355ab1d22dc40aec3bb08d9c
TayJung <tay.jung29@gmail.com>
2022-06-10 23:09:29 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let idade = 21
+
+if( idade > 18) {
+    console.log('Você já pode dirigir!')
+}
+
+
+let cafe = true
+
+if( cafe){
+    console.log('esta quente pode beber')
+}else{
+    console.log('aquecer o café')
+}
+
+
+let aniversario = 'janeiro'
+
+if(aniversario =='janeiro' || aniversario == 'desembro'){
+    console.log('Parabéns, você esta perto do seu aniversário!')
+}else{
+    console.log('Não esta no mês do seu aniversario, tente no próximo mês! ')
+}
+
+
+let nome = 'Ronaldo'
+
+if(nome == 'Ronaldo'){
+    console.log('seu nome começa com R')
+}else{
+    console.log('seu nome começa com outra letra')
+}
+
